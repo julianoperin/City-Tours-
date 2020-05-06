ca15b5f1e1b0df7bbe33392e96ee65c555fc528a
‘Juliano <julianoperins@gmail.com>
2020-05-06 19:01:24 -0400

    Finished the tour component

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -7,15 +7,22 @@ export default class TourList extends Component {
   state = {
     tours: tourData
   };
+  removeTour = id => {
+    const { tours } = this.state;
+    const sortedTours = tours.filter(tour => tour.id !== id);
+    this.setState({
+      tours: sortedTours
+    });
+  };
+
   render() {
     const { tours } = this.state;
-    console.log(tours);
 
     return (
       <section className="tourList">
-        {tours.map(tour => {
-          return <Tour key={tour.id} tour={tour} />;
-        })}
+        {tours.map(tour => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+        ))}
       </section>
     );
   }
